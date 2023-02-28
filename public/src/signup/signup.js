791542a8d086b626a5e3c5a34107d252559176be
moraPresence <od34156la@mail.ru>
2023-02-28 23:18:12 +0300

    signup added

--- public/src/signup/signup.js
+++ public/src/signup/signup.js
@@ -1,4 +1,4 @@
-function renderLogin(parent) {
+function renderSignUp(parent) {
     const container = document.createElement('div');
     container.classList.add("container");
     const mainSide = document.createElement('div');
@@ -20,12 +20,39 @@ function renderLogin(parent) {
     wrapperIcon.appendChild(iconParagraph)
 
 
-    const titleAuth = createParagraph("Авторизация")
+    const titleAuth = createParagraph("Создание почтового ящика")
     titleAuth.classList.add("font-title-1", "login-title")
 
 
     const form = document.createElement('form')
     formAddParam(form, "#", "get", "multipart/form-data")
+
+
+    const wrapperInputName = document.createElement('div');
+    wrapperInputName.classList.add("wrapper-input")
+
+    const nameInput = createInput('text', 'first-name', "", 3, 20, true);
+    nameInput.classList.add("wrapper-input-input", "wrapper-input-color");
+
+    const nameSpan = createSpan("Имя")
+    nameSpan.classList.add("font-normal-1", "span-wrapper", "wrapper-input-span")
+
+    wrapperInputName.appendChild(nameInput);
+    wrapperInputName.appendChild(nameSpan);
+
+
+    const wrapperInputSurname = document.createElement('div');
+    wrapperInputSurname.classList.add("wrapper-input")
+
+    const surnameInput = createInput('text', 'last-name', "", 3, 20, true);
+    surnameInput.classList.add("wrapper-input-input", "wrapper-input-color");
+
+    const surnameSpan = createSpan("Фамилия")
+    surnameSpan.classList.add("font-normal-1", "span-wrapper", "wrapper-input-span")
+
+    wrapperInputSurname.appendChild(surnameInput);
+    wrapperInputSurname.appendChild(surnameSpan);
+
 
     const wrapperInputEmail = document.createElement('div')
     wrapperInputEmail.classList.add("wrapper-input")
@@ -53,47 +80,43 @@ function renderLogin(parent) {
     wrapperInputPassword.appendChild(passSpan);
 
 
-    const checkBox = document.createElement('div');
-    checkBox.classList.add("remember")
+    const wrapperInputPassword2 = document.createElement('div');
+    wrapperInputPassword2.classList.add("wrapper-input")
 
-    const checkBoxLabel = createLabel()
+    const passInput2 = createInput('password', 'password', "", 3, 20, true);
+    passInput2.classList.add("wrapper-input-input", "wrapper-input-color");
 
-    const checkBoxInput = createInput("checkbox", "remember", "yes")
-    const checkBoxSpan = createSpan("Запомнить")
-    checkBoxSpan.classList.add("font-normal-1")
+    const passSpan2 = createSpan("Повторите пароль")
+    passSpan2.classList.add("font-normal-1", "span-wrapper", "wrapper-input-span")
 
-    checkBoxLabel.appendChild(checkBoxInput)
-    checkBoxLabel.appendChild(checkBoxSpan)
-
-    const checkBoxLink = createLink("Забыли пароль?", "#")
-    checkBoxLink.classList.add("font-normal-1", "signup-link")
-
-    checkBox.appendChild(checkBoxLabel)
-    checkBox.appendChild(checkBoxLink)
+    wrapperInputPassword2.appendChild(passInput2);
+    wrapperInputPassword2.appendChild(passSpan2);
 
 
     const wrapperInputButton = document.createElement('div');
     wrapperInputButton.classList.add("wrapper-input")
 
-    const submitBtn = createButton('submit', "Войти")
+    const submitBtn = createButton('submit', "Создать")
     submitBtn.classList.add("button-long", "b-color", "font-normal-1")
 
     wrapperInputButton.appendChild(submitBtn);
 
 
+    form.appendChild(wrapperInputName)
+    form.appendChild(wrapperInputSurname)
     form.appendChild(wrapperInputEmail)
     form.appendChild(wrapperInputPassword)
-    form.appendChild(checkBox)
+    form.appendChild(wrapperInputPassword2)
     form.appendChild(wrapperInputButton)
 
 
     const wrapperInputBottom = document.createElement('div');
     wrapperInputBottom.classList.add("wrapper-input-bottom");
 
-    const subtext = createParagraph("Мы еще не знакомы?");
+    const subtext = createParagraph("Мы уже знакомы?");
     subtext.classList.add("font-normal-1");
 
-    const signUpLink = createLink("Регистрация", "#",);
+    const signUpLink = createLink("Войти", "/login",);
     signUpLink.classList.add("font-normal-1", "signup-link");
 
     wrapperInputBottom.appendChild(subtext);
@@ -114,7 +137,7 @@ function renderLogin(parent) {
 
         ajax(
             'POST',
-            '/login',
+            '/signup',
             {email, password},
             status => {
                 if (status === 200) {
@@ -128,4 +151,4 @@ function renderLogin(parent) {
     parent.appendChild(container);
 }
 
-renderLogin(rootElement);
+renderSignUp(rootElement);
