db8ab1d3afa07ffde55f2c8ce7678f9a4b5252e0
moraPresence <od34156la@mail.ru>
2023-03-01 23:51:19 +0300

    handdlebars added

--- public/src/modules/validation.js
+++ public/src/modules/validation.js
@@ -2,5 +2,30 @@ const emailRegex = /@/;
 
 export default class Validation
 {
+    putErrorMessage = (target, id, text) => {
+        const err = document.createElement('div');
+        err.textContent = text;
+        err.id = id;
+        err.classList.add("error-input", "font-caption-0");
+        target.parentElement.insertAdjacentElement('beforeend', err)
+    }
 
+    validateEmail = (data) => {
+        if (!(emailRegex).test(data)) {
+            return {status: false, message: 'Неверный логин'};
+        }
+        return {status: true, message: ''};
+    };
+
+    validatePassword = (data) => {
+        if (data.length < 5) {
+            return {status: false, message: 'Пароль слишком короткий'};
+        }
+        return {status: true, message: ''};
+    };
+
+    validateRegFields = (email, password, anotherPassword = password) => {
+        return ((password === anotherPassword) && (this.validateEmail(email).status) &&
+            (this.validatePassword(password).status));
+    };
 }
--- public/src/pages/login/login.js
+++ public/src/pages/login/login.js
@@ -1,6 +1,7 @@
 import formComponent from '../../components/form/form.js';
 import basePage from '../base-page.js';
 import '../templates.js';
+import Validation from "../../modules/validation.js";
 
 
 export default class login extends basePage {
@@ -18,6 +19,58 @@ export default class login extends basePage {
         this.formComponent = new formComponent(document.getElementById('login-form'));
         this.formComponent.render(context);
 
+
+        const form = document.getElementById('login-form');
+        const fields = context.fields;
+        console.log(fields.email.name)
+        console.log(document.getElementById(fields.email.name))
+        document.getElementById(fields.email.name).focus();
+
+        const focusValidator = async (e) => {
+            const validation = new Validation()
+
+            console.log(e.target.name)
+            switch (e.target.name) {
+                case 'email':
+                    const email_check = validation.validateEmail(e.target.value);
+                    if (!email_check.status) {
+                        validation.putErrorMessage(document.getElementById(e.target.name), 'emailError', email_check.message)
+                    } else if (document.getElementById('emailError') !== null) {
+                        document.getElementById('emailError').remove()
+                    }
+                    break;
+
+                case 'password':
+                    const pass_check = validation.validatePassword(e.target.value);
+                    if (!pass_check.status) {
+                        validation.putErrorMessage(document.getElementById(e.target.name), 'passError', pass_check.message)
+                    } else if (document.getElementById('passError') !== null) {
+                        document.getElementById('passError').remove()
+                    }
+                    break;
+            }
+
+        }
+
+        const onSubmitHandler = async (e) => {
+            const data = [];
+            const validation = new Validation()
+
+            e.preventDefault()
+
+            Object.keys(fields).forEach((input) => {
+                data.push(form.querySelector(`[name=${fields[input].name}]`));
+            })
+
+            const [email, password] = data;
+            if (validation.validateRegFields(email.value, password.value)) {
+                console.log("send to /login")
+            }
+        }
+
+
+        form.addEventListener('submit', onSubmitHandler);
+        form.addEventListener('focusout', focusValidator);
     }
 }
 
