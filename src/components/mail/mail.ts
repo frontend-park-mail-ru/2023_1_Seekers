7ef1c6119b368c984950a064980291f6bff33ded
Ogrebatel <ogrebatelb@gmail.com>
2023-03-28 12:34:13 +0300

    mail button actions added

--- src/components/mail/mail.ts
+++ src/components/mail/mail.ts
@@ -4,12 +4,16 @@ import template from '@components/mail/mail.hbs'
 import {reducerLetters} from "@stores/LettersStore";
 import {MailContent} from '@uikits/mail-content/mail-content';
 import {microEvents} from "@utils/microevents";
+import {config} from "@config/config";
+import {IconButton} from "@uikits/icon-button/icon-button";
+import {dispatcher} from "@utils/dispatcher";
+import {actionGetLetters} from "@actions/letters";
 
 
 export interface Mail {
     state: {
         element: Element,
-        children: Element[],
+        actionButtons: Element[],
     },
 }
 
@@ -26,11 +30,10 @@ export class Mail extends Component {
         super(context);
         this.state = {
             element: document.createElement('div'),
-            children: [],
+            actionButtons: [],
         }
 
         this.rerender = this.rerender.bind(this);
-
         microEvents.bind('mailChanged', this.rerender);
     }
 
@@ -41,17 +44,26 @@ export class Mail extends Component {
         if(reducerLetters._storage.get(reducerLetters._storeNames.mail).title === undefined){
             this.parent.insertAdjacentHTML('afterbegin', template({}));
         } else {
+            let actionButtons = '';
+            config.buttons.mailActionButtons.forEach((button: Object) => {
+                actionButtons += (IconButton.renderTemplate(button));
+            })
             this.parent.insertAdjacentHTML('afterbegin', template(
                 {
                     from_user: reducerLetters._storage.get(reducerLetters._storeNames.mail).from_user,
                     creating_date: reducerLetters._storage.get(reducerLetters._storeNames.mail).creating_date,
                     recipient: 'ogreba@mailbox.ru',
                     mailContent: MailContent.renderTemplate(reducerLetters._storage.get(reducerLetters._storeNames.mail)),
+                    actionButtons: actionButtons,
                 }
             ));
         }
 
         this.state.element = this.parent.getElementsByClassName('mail')[0];
+
+        this.state.actionButtons = [...this.state.element.getElementsByClassName('icon-button')];
+        console.log('size: ' + this.state.actionButtons.length);
+        console.log(this.state.actionButtons);
         this.registerEventListener();
     }
 
@@ -60,6 +72,10 @@ export class Mail extends Component {
      * will unregister listeners for each button in mail
      */
     registerEventListener() {
+        this.state.actionButtons.forEach((button) => {
+            console.log('in cycle');
+            button.addEventListener('click', this.letterAction);
+        })
     }
 
     /**
@@ -67,6 +83,9 @@ export class Mail extends Component {
      * will register listeners for each button in mail
      */
     unregisterEventListener() {
+        this.state.actionButtons.forEach((button) => {
+            button.removeEventListener('click', this.letterAction);
+        })
     }
 
     /**
@@ -83,5 +102,16 @@ export class Mail extends Component {
         console.log('in rerender');
         this.purge();
         this.render();
+    }
+
+    letterAction(e: Event) {
+        e.preventDefault();
+        const {currentTarget} = e;
+        if(currentTarget instanceof HTMLElement){
+            if(currentTarget.dataset.section){
+                console.log('dispatching mail action...');
+                // dispatcher.dispatch(actionGetLetters(currentTarget.dataset.section));
+            }
+        }
     }
 }
