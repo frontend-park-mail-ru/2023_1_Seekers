dbbbd5e4bf585a2f8e4a3bf855996501d6ff8505
dyndtikj <vlamaster08@gmail.com>
2023-04-09 01:14:26 +0300

    fix prod

--- src/config/regs.ts
+++ src/config/regs.ts
@@ -1,5 +1,5 @@
 export const hrefRegExp = {
     endSlash: /\/$/,
-    host: /^\w+:.*?(:)\d|(?!(w+)\.)\w*(?:\w+\.)+\w+/,
+    host: /^\w+:.*?(:)\d/, //|(?!(w+)\.)\w*(?:\w+\.)+\w+
     localhost: /^\w+:\/\/\w+:\d|^\w+:\/\/\w+/i,
 };
