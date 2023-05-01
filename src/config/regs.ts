54fb7af6d3b214d0d827b7be7624c244883a315c
ogrebatel <ogrebatelb@gmail.com>
2023-05-01 19:52:37 +0300

    fix reg

--- src/config/regs.ts
+++ src/config/regs.ts
@@ -1,6 +1,6 @@
 export const hrefRegExp = {
     endSlash: /\/$/,
     // host: /^\w+:.*?(:)\d*/,
-    host: /(?!(w+)\.)\w*(?:\w+\.)+\w+/,
+    host: /^\w+:.*?(:)\d|^\w+:\/\/\w+.\w+/,
     localhost: /^\w+:.*?(:)\d*/,
 };
