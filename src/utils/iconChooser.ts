d1ef28efae8351db8fac9a54a0147d01bd9372af
ogrebatel <ogrebatelb@gmail.com>
2023-05-14 18:48:34 +0300

    icons

---
+++ src/utils/iconChooser.ts
@@ -0,0 +1,20 @@
+import {icons} from '@config/icons';
+
+
+class IconChooser {
+    choose = async (fileName: string) => {
+        const extention = fileName.split('.').pop();
+
+        if (extention) {
+            const foundIcon = icons.find((icon) => icon.ext === extention);
+            if (foundIcon) {
+                return foundIcon.svg;
+            } else {
+                return icons[0].svg;
+            }
+        }
+        return icons[0].svg;
+    };
+}
+
+export const iconChooser = new IconChooser();
