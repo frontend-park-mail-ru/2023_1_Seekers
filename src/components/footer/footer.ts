3973f27334f61d03469009006dfcead8c09b9bcf
MoraPresence <od34156la@mail.ru>
2023-06-02 20:59:20 +0300

    css fix

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -63,6 +63,7 @@ export class Footer extends Component {
                     document.getElementById('navbar__frame-left')?.classList.remove('navbar__frame-left__hide');
                     document.getElementById('navbar__frame-right')?.classList.remove('navbar__frame-right__hide');
                     document.getElementById('navbar__menu-button')?.classList.remove('navbar__menu-button__hide');
+                    document.getElementById('footer-button')?.classList.remove('footer-button__show');
                     await dispatcher.dispatch(actionGetMailboxPage({path: data}));
                     currentTarget.dispatchEvent(
                         new MouseEvent('click', {bubbles: true, cancelable: true}));
