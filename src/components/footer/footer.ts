49ede7de9d4d89485ae8a479f3f77246734a79ff
ogrebatel <ogrebatelb@gmail.com>
2023-06-02 19:09:41 +0300

    bottom sidebar fix

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -5,8 +5,10 @@ import {Component} from '@components/component';
 import {config} from '@config/config';
 import {reducerUser} from '@stores/userStore';
 import {dispatcher} from '@utils/dispatcher';
-import {actionLogout, actionGetAccountPage,
-    actionGetMailboxPage, actionGetProfilePage, actionGetSecurityPage} from '@actions/user';
+import {
+    actionLogout, actionGetAccountPage,
+    actionGetMailboxPage, actionGetProfilePage, actionGetSecurityPage, actionGetAnonymousPage
+} from '@actions/user';
 
 export interface Footer {
     state: {
@@ -67,6 +69,14 @@ export class Footer extends Component {
                         currentTarget.dispatchEvent(
                             new MouseEvent('click', {bubbles: true, cancelable: true}));
                         break;
+
+                    case config.buttons.sidebarButtons.anonymous.href:
+                        await dispatcher.dispatch(actionGetAccountPage({path: data}));
+                        await dispatcher.dispatch(actionGetAnonymousPage());
+                        currentTarget.dispatchEvent(
+                            new MouseEvent('click', {bubbles: true, cancelable: true}));
+                        break;
+
                     case config.buttons.sidebarButtons.logout.href:
                         await dispatcher.dispatch(actionLogout());
                         break;
